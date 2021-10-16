25212eb4a62186a7f40007680059a13f72695641
Clivaly-Cardoso <clivalydev@gmail.com>
2021-10-16 17:57:37 -0300

    Second component done

--- src/ui/components/data-display/PageTitle/PageTitle.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PageTitleContainer, PageTitleStyled } from './PageTitle.style';
+import { PageTitleContainer, PageTitleStyled, PageSubTitleStyled } from './PageTitle.style';
 
 interface PageTitleProps{
   title: string;
@@ -9,10 +9,8 @@ interface PageTitleProps{
 export const PageTitle: React.FC<PageTitleProps> = (props) => {
   return (
       <PageTitleContainer>
-        <PageTitleStyled>
-        {props.title}
-        </PageTitleStyled>
-        {props.subtitle}
+        <PageTitleStyled>{props.title}</PageTitleStyled>
+        <PageSubTitleStyled>{props.subtitle}</PageSubTitleStyled>
       </PageTitleContainer>
   );
 }
